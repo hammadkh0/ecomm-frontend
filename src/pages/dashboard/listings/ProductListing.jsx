c36a7077c90ddaf5a7eca3b7ae7a67f2b7ccf324
hammadkh0 <hammad5718@gmail.com>
2023-04-20 23:18:44 +0500

    Feat: Added Grammarly Spellcheck and Keywords Extraction

--- src/pages/dashboard/listings/ProductListing.jsx
+++ src/pages/dashboard/listings/ProductListing.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import { Button } from "@mui/material";
+import { Grammarly, GrammarlyEditorPlugin } from "@grammarly/editor-sdk-react";
+import { rake } from "rake-js/dist/lib/rake";
+
+import keyword_extractor from "keyword-extractor";
 
 import styles from "./productlisting.module.css";
 import CopyToClipboard from "../../../Component/Inputs/CopyToClipboard";
 
 const ProductListing = () => {
+  const [input, setInput] = useState("");
+  const [keywords, setKeywords] = useState("");
   const [title, setTitle] = useState("");
   const [bullet1, setBullet1] = useState("");
   const [bullet2, setBullet2] = useState("");
@@ -42,6 +48,21 @@ const ProductListing = () => {
     },
   ];
 
+  const handleInput = (e) => {
+    const words = e.target.value;
+    setInput(words);
+    if (words.length > 3) {
+      const myKeywords = keyword_extractor.extract(words, {
+        language: "english",
+        remove_digits: true,
+        return_changed_case: false,
+        remove_duplicates: true,
+        return_max_ngrams: 0,
+      });
+      setKeywords(myKeywords.join("\n"));
+    }
+  };
+
   return (
     <div>
       <h2>Enter the keywords</h2>
@@ -52,18 +73,20 @@ const ProductListing = () => {
               type="text"
               className={[styles.textInput, styles.input].join(" ")}
               placeholder="Enter some text"
+              value={input}
+              onChange={handleInput}
             />
             <textarea
-              name=""
-              rows={70}
-              cols={70}
+              value={keywords}
+              onChange={(e) => setKeywords(e.target.value)}
+              rows={7}
               className={[styles.keywordsInput, styles.input].join(" ")}
-              id=""
+              id="keywords"
               placeholder="Keywords generated"
             />
 
             <Button className={styles.applyBtn} onClick={() => {}}>
-              Apply
+              Generate Listing
             </Button>
           </form>
         </div>
@@ -75,62 +98,39 @@ const ProductListing = () => {
               <p>Title</p>
               <CopyToClipboard text={title} />
             </div>
-            <textarea
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              rows={3}
-              className="titleInput"
-            />
+
+            <GrammarlyEditorPlugin
+              clientId={`${import.meta.env.VITE_GRAMMARLY_CLIENT_ID}`}
+            >
+              <textarea
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                rows={3}
+                className="titleInput"
+              />
+            </GrammarlyEditorPlugin>
           </div>
 
           {/* -------------------- BULLETS OUTPUT ------------------------ */}
-          {bullets.map((bullet) => (
-            <div className={styles.output} key={bullet.id}>
-              <div className={styles.outputHeading}>
-                <p>{bullet.name}</p>
-                <CopyToClipboard text={bullet.value} />
+          <Grammarly clientId={`${import.meta.env.VITE_GRAMMARLY_CLIENT_ID}`}>
+            {bullets.map((bullet) => (
+              <div className={styles.output} key={bullet.id}>
+                <div className={styles.outputHeading}>
+                  <p>{bullet.name}</p>
+                  <CopyToClipboard text={bullet.value} />
+                </div>
+                <GrammarlyEditorPlugin>
+                  <textarea
+                    value={bullet.value}
+                    rows={5}
+                    className={bullet.class}
+                    id={bullet.id}
+                    onChange={bullet.setValue}
+                  />
+                </GrammarlyEditorPlugin>
               </div>
-              <textarea
-                value={bullet.value}
-                rows={5}
-                className={bullet.class}
-                id={bullet.id}
-                onChange={bullet.setValue}
-              />
-            </div>
-          ))}
-
-          {/* <div className={styles.output}>
-            <div className={styles.outputHeading}>
-              <p>Bullet1</p>
-              <CopyToClipboard />
-            </div>
-            <textarea rows={5} className="bullet" id="bullet1" />
-          </div>
-
-          <div className={styles.output}>
-            <div className={styles.outputHeading}>
-              <p>Bullet 2:</p>
-              <CopyToClipboard />
-            </div>
-            <textarea rows={5} className="bullet" id="bullet2" />
-          </div>
-
-          <div className={styles.output}>
-            <div className={styles.outputHeading}>
-              <p>Bullet 3:</p>
-              <CopyToClipboard />
-            </div>
-            <textarea rows={5} className="bullet" id="bullet3" />
-          </div>
-
-          <div className={styles.output}>
-            <div className={styles.outputHeading}>
-              <p>Bullet 4:</p>
-              <CopyToClipboard />
-            </div>
-            <textarea rows={5} className="bullet" id="bullet4" />
-          </div> */}
+            ))}
+          </Grammarly>
         </div>
       </div>
     </div>
